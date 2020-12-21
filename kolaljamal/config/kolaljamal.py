9a7f7d73438bceefeeecc39ce1aeb374ba965744
ashish-greycube <mr.ashish.shah@greycube.in>
2020-12-21 19:28:06 +0530

    report

---
+++ kolaljamal/config/kolaljamal.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Report"),
+			"items": [
+				{
+					"type": "report",
+					"name": "Salesman Itemwise Commission",
+					"is_query_report": True,
+					"doctype": "Sales Invoice"
+				}
+			]
+		}
+	]
